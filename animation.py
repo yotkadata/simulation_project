d4a153d492371bac028381845dfd0f0e6af91d1c
Jan Kühn <yotka@posteo.org>
2023-05-18 00:15:53 +0200

    Working animation (but too slow)

--- animation.py
+++ animation.py
@@ -1,6 +1,9 @@
+import time
+
 import cv2
 import numpy as np
-import time
+import pandas as pd
+from faker import Faker
 
 TILE_SIZE = 32
 
@@ -28,7 +31,7 @@ CCCCCCCCCCCCCCEEEEEE
 """.strip()
 
 
-class SupermarketMap:
+class Supermarket:
     """
     Visualizes the supermarket background.
     """
@@ -47,6 +50,12 @@ class SupermarketMap:
             (self.nrows * TILE_SIZE, self.ncols * TILE_SIZE, 3), dtype=np.uint8
         )
         self.prepare_map()
+        self.customers = []
+        self.tprobs = self.load_tprobs()
+        self.entry_times = self.load_entry_times()
+        self.last_id = 0
+        self.current_time = self.entry_times.loc[0, "timestamp"]
+        self.register = pd.DataFrame(columns=["timestamp", "customer_no", "location"])
 
     def extract_tile(self, row, col):
         """
@@ -88,13 +97,22 @@ class SupermarketMap:
         # Get all unique characters in floor plan
         chars = "".join(set(FLOOR)).replace("\n", "")
 
+        mapping = {
+            "E": "entrance",
+            "F": "fruit",
+            "D": "dairy",
+            "S": "spices",
+            "B": "drinks",
+            "C": "checkout",
+            "#": "wall",
+        }
         # Create empty dict to store possible positions
-        self.positions = {key: [] for key in chars}
+        self.positions = {mapping[key]: [] for key in chars}
 
         for row, line in enumerate(self.contents):
             for col, char in enumerate(line):
                 # Save all possible positions of letters in a dict
-                self.positions[char].append((row, col))
+                self.positions[mapping[char]].append((row, col))
 
                 # Get tile for current position
                 bm = self.get_tile(char)
@@ -115,6 +133,54 @@ class SupermarketMap:
         """
         frame[0 : self.image.shape[0], 0 : self.image.shape[1]] = self.image
 
+    def load_tprobs(self):
+        """
+        Load transition probabilities from a CSV file.
+        """
+        return pd.read_csv("data/transition_probabilities.csv", index_col=[0])
+
+    def load_entry_times(self):
+        """
+        Load entry times from a CSV file.
+        """
+        return pd.read_csv("data/entry_times.csv", index_col=[0])
+
+    def add_new_customer(self, new_customer):
+        """
+        Add one customer.
+        """
+        assert isinstance(new_customer, Customer)
+        self.customers.append(new_customer)
+
+        # Register customer action
+        self.register_action(self.current_time, new_customer.id, new_customer.section)
+
+        print(f"{new_customer.name} (ID: {new_customer.id}) entered the supermarket.")
+
+    def add_new_customers(self, num_customers):
+        """
+        Add multiple new customers.
+        """
+        for _ in range(num_customers):
+            f = Faker()
+            new_customer = Customer(self.last_id + 1, f.name(), market, tiles)
+            self.add_new_customer(new_customer)
+            self.last_id += 1
+
+    def remove_customer(self, customer):
+        """
+        Remove customer from store.
+        """
+        assert isinstance(customer, Customer)
+        self.customers.remove(customer)
+        print(f"{customer.name} (ID: {customer.id}) has left the store.")
+
+    def register_action(self, timestamp, id, section):
+        """
+        Register an action for the CSV output at the end.
+        """
+        self.register.loc[len(self.register)] = [timestamp, id, section]
+
     def write_image(self, filename):
         """
         Writes the image into a file.
@@ -123,23 +189,28 @@ class SupermarketMap:
 
 
 class Customer:
-    def __init__(self, supermarket, tiles):
+    """
+    A single customer that moves through the supermarket
+    in a MCMC simulation.
+    """
+
+    def __init__(self, id, name, supermarket, tiles, section="entrance"):
         """
-        Customer object
         supermarket: A SuperMarketMap object
         avatar : a numpy array containing a 32x32 tile image
         """
-
+        self.id = id
+        self.name = name
         self.supermarket = supermarket
-        self.row, self.col = self.get_rand_position("E")
+        self.row, self.col = self.get_rand_position("entrance")
         self.tiles = tiles
         self.avatar = self.extract_tile(7, 0)
-        self.section = "C"
+        self.section = section
 
         time.sleep(1)
 
     def __repr__(self) -> str:
-        return f"Customer object"
+        return f"<Customer {self.name}, currently in section '{self.section}'>"
 
     def get_rand_position(self, section=None):
         """
@@ -180,24 +251,45 @@ class Customer:
 
         return self.tiles[row1:row2, col1:col2]
 
-    def move(self):
+    def next_section(self, tprobs=None):
         """
-        Move a customer in the store.
+        Propagates the customer to the next state.
+        Returns nothing.
         """
+        if tprobs == None:
+            tprobs = self.supermarket.tprobs
+
+        current = self.section
+
+        self.section = np.random.choice(tprobs.columns, p=tprobs.loc[self.section])
+
         self.row, self.col = self.get_rand_position()
+
+        if current == self.section:
+            print(f"{self.name} (ID: {self.id}) stayed in {current}.")
+        else:
+            print(
+                f"{self.name} (ID: {self.id}) moved from {current} to {self.section}."
+            )
+
+    def is_active(self):
+        """
+        Returns True if the customer has not reached the checkout yet.
+        """
+
+        return self.section != "checkout"
 
 
 if __name__ == "__main__":
     tiles = cv2.imread("tiles.png")
 
     # Instantiate Supermarket object
-    market = SupermarketMap(FLOOR, tiles)
-
-    # Instantiate customer object
-    customer = Customer(market, tiles)
+    market = Supermarket(FLOOR, tiles)
 
     # Create background of the same size as the supermarket
     background = np.zeros(market.image.shape, np.uint8)
+
+    i = 0
 
     while True:
         # Create a new frame
@@ -209,17 +301,37 @@ if __name__ == "__main__":
         # Draw the Supermarket
         market.draw(frame)
 
-        # Draw the customer
-        customer.draw(frame)
-        time.sleep(1)
+        market.current_time = market.entry_times.loc[i, "timestamp"]
 
-        # Move the customer
-        customer.move()
+        for c in market.customers:
+            # Move customers currently in the store to next section
+            c.next_section()
+            market.register_action(market.current_time, c.id, c.section)
+
+            # Draw the customer
+            c.draw(frame)
+
+            # Remove customers that reached checkout
+            if not c.is_active():
+                market.remove_customer(c)
+
+        # Add new customers entering the store
+        market.add_new_customers(market.entry_times.loc[i, "new_customers"])
+
+        print(f"Currently there are {len(market.customers)} customers in the store.\n")
+
+        i += 1
+
+        if i == len(market.entry_times):
+            break
 
         if key == 113:  # 'q' key
             break
 
         cv2.imshow("frame", frame)
+
+    # Save register to CSV file
+    market.register.to_csv("data/simulation_results_anim.csv")
 
     cv2.destroyAllWindows()
 
